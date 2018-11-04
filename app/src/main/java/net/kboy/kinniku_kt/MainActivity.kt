c512a30da497164f37480e35e192652640919a5d
Kei Fujikawa <fujikei22funk@gmail.com>
2018-11-04 23:20:31 +0900

    change drawer

--- app/src/main/java/net/kboy/kinniku_kt/MainActivity.kt
+++ app/src/main/java/net/kboy/kinniku_kt/MainActivity.kt
@@ -1,23 +1,45 @@
 package net.kboy.kinniku_kt
 
-import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        val strings = resources.getStringArray(R.array.menu_string_array)
-        val adapter = ArrayAdapter<String>(this, R.layout.list_row, strings)
-        navigation_menu_list.adapter = adapter
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        navigation_menu_list.setOnItemClickListener { _, _, position, _ ->
-            drawer_layout.closeDrawers()
+        nav_view.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_camera -> {
+                    // Handle the camera action
+                }
+                R.id.nav_gallery -> {
+
+                }
+                R.id.nav_slideshow -> {
+
+                }
+                R.id.nav_manage -> {
+
+                }
+                R.id.nav_share -> {
+
+                }
+            }
+            drawer_layout.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
         }
     }
 }
