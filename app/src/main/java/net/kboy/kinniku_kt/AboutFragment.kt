a6a392352a0a0984b7008dae6f252945047d0a88
Kei Fujikawa <fujikei22funk@gmail.com>
2018-11-06 08:41:05 +0900

    show WebView

--- app/src/main/java/net/kboy/kinniku_kt/AboutFragment.kt
+++ app/src/main/java/net/kboy/kinniku_kt/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
 
@@ -14,5 +15,11 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webView.loadUrl("https://kinniku-swift.connpass.com/event/99895/")
     }
 }
