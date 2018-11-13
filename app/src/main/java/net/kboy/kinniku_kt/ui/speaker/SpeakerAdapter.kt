d67cd9839e29467952bfede0e89701f46e1906e1
Kei Fujikawa <fujikei22funk@gmail.com>
2018-11-13 09:36:30 +0900

    fix speaker layout

--- app/src/main/java/net/kboy/kinniku_kt/ui/speaker/SpeakerAdapter.kt
+++ app/src/main/java/net/kboy/kinniku_kt/ui/speaker/SpeakerAdapter.kt
@@ -3,6 +3,7 @@ package net.kboy.kinniku_kt.ui.speaker
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import net.kboy.kinniku_kt.R
 import net.kboy.kinniku_kt.data.Speaker
 
@@ -24,9 +25,13 @@ class SpeakerAdapter : RecyclerView.Adapter<SpeakerViewHolder>() {
     override fun onBindViewHolder(holder: SpeakerViewHolder, position: Int) {
         val speaker = items[position]
         holder.name.text = speaker.name
-        holder.screenName.text = speaker.screenName
+        holder.screenName.text = "@" + speaker.screenName
         holder.title.text = speaker.title
         holder.reps.text = speaker.point + "reps"
+
+        Picasso.get()
+            .load(speaker.imgURL)
+            .into(holder.image)
     }
 
     override fun getItemCount(): Int = items.size
